7a65bebc1b37c52a071c4540be71858588d85a89
justin-lim-335 <justinlim0223@gmail.com>
2025-10-14 20:26:58 -0700

    Fixing bugs

--- server/supabase.ts
+++ server/supabase.ts
@@ -7,7 +7,7 @@ const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
-  throw new Error('Missing Supabase URL or service role key!' + SUPABASE_SERVICE_ROLE_KEY + "." + SUPABASE_URL);
+  throw new Error('Missing Supabase URL or service role key!');
 }
 
 export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
