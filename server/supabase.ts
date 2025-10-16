43f394398a639d50db26bdd0a403d7f468b85fa9
justin-lim-335 <justinlim0223@gmail.com>
2025-10-15 20:09:54 -0700

    changing up backend

--- server/supabase.ts
+++ server/supabase.ts
@@ -3,11 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from "dotenv"
 
 dotenv.config();
-const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error('Missing Supabase URL or service role key!' + SUPABASE_URL);
-}
+}up 
 
 export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
