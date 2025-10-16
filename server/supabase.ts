fab633489f4b22c86aed133d908776218bf51954
justin-lim-335 <justinlim0223@gmail.com>
2025-10-15 20:10:20 -0700

    updating backend

--- server/supabase.ts
+++ server/supabase.ts
@@ -8,6 +8,6 @@ const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error('Missing Supabase URL or service role key!' + SUPABASE_URL);
-}up 
+}
 
 export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
