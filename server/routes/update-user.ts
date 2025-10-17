1ce6febb3f3d9f824c5ed61618fa088f8f3606e9
justin-lim-335 <justinlim0223@gmail.com>
2025-10-17 03:22:03 -0700

    linking to correct URL

--- server/routes/update-user.ts
+++ server/routes/update-user.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export default async function updateUserHandler(req: Request, res: Response) {
